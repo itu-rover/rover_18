989b7e1b0ac29877772fcdd8633f087b1148d666
ferhatm <ferhat.m.41@gmail.com>
2017-11-23 17:13:17 +0300

    Adds rover_serial package

---
+++ rover_serial/scripts/rover_serial.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import rospy
+import serial
+import time
+from std_msgs.msg import String
+
+namespace = '[RoverSerial : ] '
+serialMsg = ""
+
+#get data from pc
+def serialCallback(data):
+    global serialMsg
+    #print("I heard : " + data.data)
+    serialMsg = data.data
+
+
+def letsSerial():
+    rospy.init_node("rover_serial")
+    rospy.Subscriber('rover_serial_topic',String,serialCallback)
+    sensor_pub = rospy.Publisher('rover_serial_sensor',String,queue_size=10)
+
+    global namespace
+    global serialMsg
+    printOnce = True
+    while not rospy.is_shutdown():
+
+        ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS) #open serial
+        ser.timeout = 1
+        ser2 = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+                            bytesize=serial.EIGHTBITS)  # open serial
+        ser2.timeout = 1
+
+
+        rospy.loginfo(ser.portstr)
+        rospy.loginfo(ser2.portstr)
+
+        while ser.isOpen() and ser2.is_open():
+            if printOnce == True:
+
+                print(namespace + "serial is open")
+                print(namespace + "serial2 is open")
+                printOnce = False
+
+
+            ser.writelines(serialMsg + "\n")
+
+
+
+            receive = ser.readline()
+            receive2 = ser2.readline()
+            sensor_pub.publish(receive)
+            sensor_pub2.publish(receive2)
+            print(namespace + "I'm reading this from serial : " + str(receive) + "   I'm writing to serial :  "  + serialMsg + "\n")
+            print(namespace + "I'm reading this from serial : " + str(receive2) +"\n")
+
+            ser.flushInput()
+            ser.flushOutput()
+            ser2.flushInput()
+            ser2.flushOutput()
+
+            time.sleep(0.4)
+        rospy.spin()
+
+
+
+if __name__ == '__main__':
+    try:
+        letsSerial()
+    except rospy.ROSInterruptException:
+        pass
